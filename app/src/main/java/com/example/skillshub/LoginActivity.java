99f594fbdf5e75f0aab866b9c9d0e3ea148520a9
Pranee99 <pranithye123@gmail.com>
2024-10-16 13:16:48 +0530

    Did some modifications

--- app/src/main/java/com/example/skillshub/LoginActivity.java
+++ app/src/main/java/com/example/skillshub/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.skillshub;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -11,17 +12,23 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
 
     private EditText emailEditText;
     private EditText passwordEditText;
-    private CheckBox rememberMeCheckBox;
     private Button loginButton;
     private TextView forgotPasswordTextView;
     private TextView signUpTextView;
+    FirebaseAuth auth;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -30,9 +37,9 @@ public class LoginActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
 
+        auth = FirebaseAuth.getInstance();
         emailEditText = findViewById(R.id.email);
         passwordEditText = findViewById(R.id.password);
-        rememberMeCheckBox = findViewById(R.id.remember_me);
         loginButton = findViewById(R.id.login_button);
         forgotPasswordTextView = findViewById(R.id.forgot_password);
         signUpTextView = findViewById(R.id.text3);
@@ -43,12 +50,30 @@ public class LoginActivity extends AppCompatActivity {
                 String email = emailEditText.getText().toString();
                 String password = passwordEditText.getText().toString();
 
-                if (email.isEmpty() || password.isEmpty()) {
-                    Toast.makeText(LoginActivity.this, "Please enter both email and password", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(LoginActivity.this, "Logging in...", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(LoginActivity.this, clientHome.class);
-                    startActivity(intent);
+                if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    if(!password.isEmpty()){
+                        auth.signInWithEmailAndPassword(email, password)
+                                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                    @Override
+                                    public void onSuccess(AuthResult authResult) {
+                                        Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(LoginActivity.this, clientHome.class));
+                                        finish();
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(LoginActivity.this, "Invalid User.Please Signup", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    }else {
+                        passwordEditText.setError("Password cannot be empty");
+                    }
+
+                } else if (email.isEmpty()) {
+                    emailEditText.setError("Email cannot be empty");
+                }else {
+                    emailEditText.setError("Please enter a valid email");
                 }
             }
         });
