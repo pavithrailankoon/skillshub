d8877e3753d2e947d358bdf2f58b501e194f1436
prasad4365 <prasadmadurangawishuddiharaba@gmail.com>
2024-10-21 11:54:52 +0530

    create some modifications for client profile

--- app/src/main/java/com/example/skillshub/ClientProfileActivity.java
+++ app/src/main/java/com/example/skillshub/ClientProfileActivity.java
@@ -1,6 +1,7 @@
 package com.example.skillshub;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,27 +9,40 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.activity.ComponentActivity;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
 
 import com.example.skillshub.model.CustomDialog;
 
 import org.w3c.dom.Text;
 
 
-public class ClientProfileActivity extends AppCompatActivity implements CustomDialog.CustomDialogInterface{
+public class ClientProfileActivity extends ComponentActivity implements CustomDialog.CustomDialogInterface{
 
     ImageView backBtn;
-    Button logOut,nameEditBtn;
-    TextView newName;
+    Button logOut,editDetails,editPassowrd;
+    TextView newName,newPhoneNumber,newAddressLine1,newAddressLine2;
+
+    private static final int REQUEST_IMAGE_GALLERY = 1;
+    private static final int REQUEST_IMAGE_CAMERA = 2;
+    private Uri imageUri;
+    private Uri DEFAULT_IMAGE_URI;
+
+    ChangePassword changePassword = new ChangePassword();
 
     public void openDialog(View view){
         CustomDialog customDialog = new CustomDialog();
         customDialog .show(getSupportFragmentManager(),"Test Customdialog");
 
+    }
+
+    private FragmentManager getSupportFragmentManager() {
+        return null;
     }
 
     @Override
@@ -44,7 +58,14 @@ public class ClientProfileActivity extends AppCompatActivity implements CustomDi
 
         backBtn = findViewById(R.id.backBtn);
         logOut = findViewById(R.id.logOut);
+        editDetails = findViewById(R.id.editDetailsBtn);
+        editPassowrd = findViewById(R.id.editPassword);
+
         newName = findViewById(R.id.name);
+        newPhoneNumber = findViewById(R.id.phoneNumber);
+        newAddressLine1 = findViewById(R.id.addressLine1);
+        newAddressLine2 = findViewById(R.id.addressLine2);
+
 
 
         backBtn.setOnClickListener(new View.OnClickListener() {
@@ -64,11 +85,18 @@ public class ClientProfileActivity extends AppCompatActivity implements CustomDi
         });
 
 
+
+
     }
 
 
     @Override
-    public void applyTexts(String name) {
+    public void applyTexts(String name, String phoneNumber,String addressLine1,String addressLine2) {
         newName.setText(name);
+        newPhoneNumber.setText(phoneNumber);
+        newAddressLine1.setText(addressLine1);
+        newAddressLine2.setText(addressLine2);
+
+
     }
 }
