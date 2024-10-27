0c4757b97d0d3977dacb5e09eb7cf935f87ca767
pavithrailankoon <ilankoonpavithra@gmail.com>
2024-10-27 16:55:08 +0530

    Update signup & login

--- app/src/main/java/com/example/skillshub/LoginActivity.java
+++ app/src/main/java/com/example/skillshub/LoginActivity.java
@@ -78,11 +78,8 @@ public class LoginActivity extends AppCompatActivity {
         signUpTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (validateInput()){
-                    Intent intent = new Intent(LoginActivity.this, AuthenticateActivity.class);
-                    startActivity(intent);
-                }
-
+                Intent intent = new Intent(LoginActivity.this, AuthenticateActivity.class);
+                startActivity(intent);
             }
         });
 
